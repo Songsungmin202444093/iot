921d7883240e2d5a7f80f04336d52bae456ad4ab
Songsungmin202444093 <163081399+Songsungmin202444093@users.noreply.github.com>
2025-04-15 15:16:18 +0900

    Update LedToggle.cpp

--- LedToggle.cpp
+++ LedToggle.cpp
@@ -2,12 +2,25 @@
 
 LedToggle::LedToggle(int pin) {
 	_pin = pin;
+	_delayTime = 0;
 	_state = false;
+	pinMode(_pin, OUTPUT);
+	digitalWrite(_pin, LOW);
+} // 수정 혹은 삭제  
+
+LedToggle::LedToggle(int pin, unsigned long delayTime) {
+	_pin = pin;
+	_state = false;
+	_delayTime = delayTime;
 	pinMode(_pin, OUTPUT);
 	digitalWrite(_pin, LOW);
 } 
 
 void LedToggle::toggle() {
+	if(_delayTime > 0) {
+		delay(_delayTime);
+	}
+	
 	_state = !_state;
 	digitalWrite(_pin, _state ? HIGH : LOW);
 }
